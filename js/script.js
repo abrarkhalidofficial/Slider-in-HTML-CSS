aac69b677103aefe1ef06f7cb4cecd1c5e650ec6
M Abrar Khalid <abrarprince471@gmail.com>
2022-07-19 01:18:33 -0700

    swiper

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".tesmonianl_row swiper-wrapper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
